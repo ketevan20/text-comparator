ca7a3a90dccb19fd5bc636ac7c30a8f1c02ef06b
ketevan20 <ketevansamukashvili@gmail.com>
2026-03-03 21:32:35 +0400

    feat: add main layout container

--- project/src/components/molecules/Header/Header.tsx
+++ project/src/components/molecules/Header/Header.tsx
@@ -1,11 +1,22 @@
+import { navItems } from "../../atoms/NavItems/NavItems"
 
 const Header = () => {
-  return (
-    <div className="flex md:hidden bg-[rgba(19,36,80,1)] px-8 py-3 justify-between items-center">
-        <img src="/images/logo.svg" alt="logo"/>
-        <img src="/images/menu.svg" alt="menu" />
-    </div>
-  )
+    return (
+        <div className="md:hidden block">
+            <div className="flex  bg-[rgba(19,36,80,1)] px-5 md:px-8 py-3 justify-between items-center">
+                <img src="/images/logo.svg" alt="logo" />
+                <img src="/images/menu.svg" alt="menu" />
+            </div>
+            <div className="px-5 md:px-8 py-4">
+                <div className="flex gap-1 items-center text-[rgba(19,36,80,1)]">
+                    {navItems[1].icon}
+                    <p className="font-bold text-[12px] leading-5">{navItems[1].label}</p>
+                    <img src="/images/dropdown.svg"/>
+                </div>
+            </div>
+            <div className="w-full h-px bg-[rgba(237,237,237,1)]"></div>
+        </div>
+    )
 }
 
 export default Header
--- project/src/components/organisms/DashboardLayout/DashboardLayout.tsx
+++ project/src/components/organisms/DashboardLayout/DashboardLayout.tsx
@@ -1,11 +1,13 @@
 import Header from "../../molecules/Header/Header"
+import MainContainer from "../../molecules/MainContainer/MainContainer"
 import Sidebar from "../../molecules/Sidebar/Sidebar"
 
 const DashboardLayout = () => {
   return (
-    <div>
+    <div className="flex md:flex-row flex-col">
         <Sidebar />
         <Header />
+        <MainContainer />
     </div>
   )
 }
